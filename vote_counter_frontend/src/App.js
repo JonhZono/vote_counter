35da832334b55f91191ce95c5727f589d31d8cd3
Jonh <jonhzono@gmail.com>
2021-03-27 15:08:42 +0900

    Add M-UI-Icon

--- vote_counter_frontend/src/App.js
+++ vote_counter_frontend/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 
 function App() {
-  return (
-    <h2>Vote Counting System</h2>
-  );
+  return <h2>Vote Counting System</h2>;
 }
 
 export default App;
